836ac2f9dbb3e3bad4f0d206dfa71fe503fcd7bb
nola0002 <nola0002@stud.ek.dk>
2026-05-20 13:30:02 +0200

    Lavede første carrousel på explore siden, og tilføjet billede til explore side.

---
+++ app/src/main/java/com/bralogrithm/never_another/view/screens/explore/cardSection.kt
@@ -0,0 +1,132 @@
+package com.bralogrithm.never_another.view.screens.explore
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bralogrithm.never_another.R
+import com.bralogrithm.never_another.model.ExploreCardSectionText
+
+@Composable
+fun CardSection(
+    listOfCardSectionText: List<ExploreCardSectionText>,
+    selectedCarrouselText: Int,
+    carrouselBackButtonClick: () -> Unit,
+    carrouselForwardButtonClick: () -> Unit
+) {
+    val shape = RoundedCornerShape(16.dp)
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ButtonCardSection(
+            image = R.drawable.backlogocardsection,
+            contentDescription = "Back logo for card Carrousel",
+            onClick = carrouselBackButtonClick
+        )
+
+        Card(
+            modifier = Modifier
+                .height(246.dp)
+                .width(313.dp)
+                .padding(vertical = 4.dp)
+                .background(Color.White)
+                .border(1.dp, Color.Black, shape),
+            shape = shape,
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+
+                    TransparentButtonCardSection(
+                        onClick = carrouselBackButtonClick
+                    )
+
+
+
+                    Column(
+                        modifier = Modifier
+                            .height(180.dp)
+                            .width(240.dp)
+                    ) {
+
+                        Text(
+                            modifier = Modifier
+                                .padding(top = 16.dp),
+                            text = listOfCardSectionText[selectedCarrouselText].thickText,
+                            textAlign = TextAlign.Center,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.ExtraBold,
+
+
+                            )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Text(
+                            text = listOfCardSectionText[selectedCarrouselText].thinText,
+                            textAlign = TextAlign.Center,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Normal,
+                        )
+                    }
+
+
+                    TransparentButtonCardSection(
+                        onClick = carrouselForwardButtonClick
+                    )
+
+                }
+            }
+        }
+
+        ButtonCardSection(
+            image = R.drawable.forwardlogocardsection,
+            contentDescription = "Forward logo for card carrousel",
+            onClick = carrouselForwardButtonClick
+        )
+
+    }
+}
